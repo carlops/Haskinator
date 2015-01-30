37e8fd6bbf31fc8e08c9ac77fc5c30d994b035c8
Basuco <alejandro.g.206@gmail.com>
2015-01-29 23:41:45 -0430

    Funciones faileando

--- proy1.hs
+++ proy1.hs
@@ -1,3 +1,5 @@
+import Data.Maybe
+
 data Oraculo = Prediccion String | Pregunta String Oraculo Oraculo deriving Read
 
 instance Show Oraculo where
@@ -26,7 +28,7 @@ pregunta _ = error("Se esperaba pregunta")
 
 positivo :: Oraculo -> Oraculo
 positivo (Pregunta _ pos _ )= pos 
-positivo _ = error("Se esperaba pregunta")
+positivo _ = error("Se esperaba prgunta")
 
 negativo :: Oraculo -> Oraculo
 negativo (Pregunta _ _ neg) = neg 
@@ -43,10 +45,46 @@ obtenerCadena (Pregunta preg izq der) pred =
 		else Nothing
 
 
-main = printMenu
+persistir :: Maybe Oraculo -> IO()
+persistir ora = do
+		putStrLn "Introduzca un nombre para el archivo donde se guardara el oraculo"
+		namefile <- getLine
+		writeFile namefile (show ora)
+		putStrLn "Persistencia completada"
+{--
+cargar :: Maybe Oraculo
+cargar = do
+	putStrLn "Introduzca un nombre del archivo que desear cargar"
+	namefile <- getLine
+	ora = Just (read (readFile name)) 
+	putStrLn "Carga completada"
+	return ora
+ --}
 
-printMenu = do 
-	putStrLn ("1  Crear un oráculo nuevo ")
+preguntaCrucial :: Maybe Oraculo -> IO()
+preguntaCrucial Nothing = putStrLn "No se pueden realizar consultas, Oraculo vacio"
+preguntaCrucial Just ora = do
+				putStrLn "Introduzca Primera prediccion: "
+			    	pred1 <- getLine
+				putStrLn "Introduzca Segunda prediccion: "
+			    	pred2 <- getLine
+				cadena1 <- obtenerCadena ora pred1
+				cadena2 <- obtenerCadena ora pred2
+				if (cadena1==Nothing || cadena2==Nothing) then putStrLn "Consulta Invalida"
+				else  putStrLn "Consulta Invalida"
+		{--			filtrado <- ((\c1 c2 -> [x | x <- c1, any (\a -> (fst a) == (fst x)) c2])) <$> cadena1 <*> cadena2
+					if  (filtrado == Just []) then
+					 	putStrLn "Consulta Invalida"
+					else
+						putStrLn fromJust (fmap (fst . last) filtrado)
+
+--}
+
+
+main = printmenu
+
+printmenu = do 
+	putStrLn ("1  crear un oráculo nuevo ")
 	putStrLn ("2: Predicción ")
 	putStrLn ("3: Persistir ")
 	putStrLn ("4: Cargar ")
